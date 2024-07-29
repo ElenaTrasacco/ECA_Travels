05bd752463096797809a1b489ede401e988a3df9
Elena Trasacco <elenatrasacco@gmail.com>
2024-07-29 17:20:32 +0200

    fine scaffolding, fortify, livewire

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,41 +1,48 @@
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg fixed-top shadow-5-strong navCustom">
-    <!-- Container wrapper -->
+<nav class="navbar navbar-expand-lg navCustom fixed-top justify-content-between">
     <div class="container-fluid">
-      <!-- Navbar brand -->
-      <a class="navbar-brand" href="#">Brand</a>
-  
-      <!-- Toggle button -->
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i class="fas fa-bars"></i>
+      <a class="navbar-brand" href="#">
+        <i class="fa-solid fa-route fa-2x" style="color: #187af1;"></i>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
       </button>
-  
-      <!-- Collapsible wrapper -->
+
+
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Left links -->
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active navColor" aria-current="page" href="{{route('homepage')}}">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active navColor" aria-current="page" href="#">Inserisci un annuncio</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active navColor" aria-current="page" href="#">Lista annunci</a>
           </li>
         </ul>
-        <!-- Left links -->
-      </div>
-      <!-- Collapsible wrapper -->
     </div>
-    <!-- Container wrapper -->
+
+        <div class="navbar-nav ">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5">
+            <li class="nav-item">
+                <a class="nav-link active navColor" aria-current="page" href="#">Registrati</a>
+              </li>
+            <li class="nav-item">
+                <a class="nav-link active navColor" aria-current="page" href="#">Login</a>
+              </li>
+              {{-- <li class="nav-item">
+                <a class="nav-link active navColor" aria-current="page" href="#">Logout</a>
+              </li> --}}
+        </ul>
+        </div>
+
+
+
+
+        {{-- <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form> --}}
+      
+    </div>
   </nav>
-  <!-- Navbar -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
-    <div class="container altezza">
-        <div class="row">
-            <div class="col-12">
+    <div class="container altezza mt-5">
+        <div class="row ">
+            <div class="col-12 mt-5">
                 <h1>Ciao a tutti</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore eveniet deleniti saepe porro voluptatum a eius, eligendi, corporis suscipit et beatae autem iste architecto reprehenderit ipsam, quasi quisquam facilis. Similique.</p>
             </div>
