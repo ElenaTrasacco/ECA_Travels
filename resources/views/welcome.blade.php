d5e5c98803960b40105954c17f55aff74dfc1093
Elena Trasacco <elenatrasacco@gmail.com>
2024-07-29 16:28:51 +0200

    scaffolding iniziale

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <div class="container altezza">
+        <div class="row">
+            <div class="col-12">
+                <h1>Ciao a tutti</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore eveniet deleniti saepe porro voluptatum a eius, eligendi, corporis suscipit et beatae autem iste architecto reprehenderit ipsam, quasi quisquam facilis. Similique.</p>
+            </div>
+        </div>
+    </div>
+    
+</x-layout>
